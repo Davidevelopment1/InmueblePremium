3fe8d35ef399e30d9c41e1950efa24e6310c5123
David <143103441+Davidevelopment1@users.noreply.github.com>
2023-12-14 12:55:26 -0400

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+//Obtener los elementos donde se mostraran los datos
+const clientesElement = document.getElementById('clientes');
+const apartamentosElement = document.getElementById('apartamentos');
+const condominiosElement = document.getElementById('condominios');
+
+fetch('Archivo.txt')
+	.then(response => response.text())
+	.then(data => {
+		
+		const values = data.split(',').map(item => parseInt(item.trim()));
+		
+		clientesElement.textContent = values[0];
+		apartamentosElement.textContent = values[1];
+		condominiosElement.textContent = values[2];
+	})
+	.catch(error => console.error('Error al obtener el archivo: ', error));
